3e65b154f79f55d4bea3690b10f31983491ad90a
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-01-20 19:05:08 +0100

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,48 @@
+
+/**
+ * @param {number} val
+ */
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+
+    if (head === null || head.next === null) {
+        return null;
+    }
+
+    let slow = head;
+    let fast = head;
+    let meetingNode = null;
+
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (fast === slow) {
+            meetingNode = fast;
+            break;
+        }
+    }
+
+    return meetingNode === null ? null : findStartNodeOfCycle(head, meetingNode);
+};
+
+/**
+ * @param {ListNode} head
+ * @param {ListNode} meetingNode
+ * @return {ListNode}
+ */
+function findStartNodeOfCycle(head, meetingNode) {
+    let current = head;
+    while (current !== meetingNode) {
+        current = current.next;
+        meetingNode = meetingNode.next;
+    }
+    return meetingNode;
+}
